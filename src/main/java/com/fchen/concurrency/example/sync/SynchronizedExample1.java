d8981e0f4b7383b83c2218333d12dc6164b2f93c
Fchen <fc_2012@163.com>
2019-04-29 20:16:47 +0800

    Synchronized

---
+++ src/main/java/com/fchen/concurrency/example/sync/SynchronizedExample1.java
@@ -0,0 +1,13 @@
+package com.fchen.concurrency.example.sync;
+
+/**
+ * @Classname SynchronizedExample1
+ * @Description SynchronizedExample1
+ * @Date 2019/4/29 20:13
+ * @Author by Fchen
+ */
+public class SynchronizedExample1 {
+    public void test1(){
+
+    }
+}
