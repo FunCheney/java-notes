0b63ca4592828981433dbb6fe08bdea85fef5e8c
FunCheney <fc_2012@163.com>
2019-05-19 22:42:04 +0800

    CyclicBarrier

---
+++ src/main/java/com/fchen/concurrency/example/aqs/CyclicBarrierStudy2.java
@@ -0,0 +1,47 @@
+package com.fchen.concurrency.example.aqs;
+
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.concurrent.CyclicBarrier;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+/**
+ * @Classname CyclicBarrierStudy
+ * @Description CyclicBarrier类学习
+ * @Date 2019/5/19 21:57
+ * @Author by Chen
+ */
+@Slf4j
+public class CyclicBarrierStudy2 {
+    //给定一个值，确定当前有多少个线程同步等待
+    //这里如果实现了Runnable接口，则Runnable先执行
+    private  static CyclicBarrier barrier = new CyclicBarrier(5,()->{
+        log.info("callback is running");
+    });
+    public static void main(String[] args) throws Exception{
+        ExecutorService executor = Executors.newCachedThreadPool();
+
+        for (int i = 0; i < 10; i++){
+            final int threadNum = i;
+            Thread.sleep(1000);
+            executor.execute(()->{
+                try {
+                    race(threadNum);
+                } catch (Exception e) {
+                    log.error("Exception:{}",e);
+                }
+            });
+        }
+        executor.shutdown();
+    }
+
+    private static void race(int threadNum) throws Exception{
+        Thread.sleep(1000);
+        log.info("{} is ready",threadNum);
+        //每一个线程调用自己的await()方法，当达到定义的数目之后，
+        // await后满的操作就可以执行了
+        barrier.await();
+        log.info("{} continue",threadNum);
+    }
+}
