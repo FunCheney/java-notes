35347b07e44c6c26c119f5af4c722f5c0c77f69d
FunCheney <fc_2012@163.com>
2019-08-01 08:44:59 +0800

    JUC源码阅读之AQS 使用用例

---
+++ src/main/java/com/fchen/concurrency/src/aqsexample/Mutex.java
@@ -0,0 +1,60 @@
+package com.fchen.concurrency.src.aqsexample;
+
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.locks.AbstractQueuedSynchronizer;
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.Lock;
+
+/**
+ * @Classname Mutex
+ * @Description
+ * @Date 2019/8/1 8:22
+ * @Author by Chen
+ */
+public class Mutex implements Lock{
+    private static class sync extends AbstractQueuedSynchronizer{
+        @Override
+        protected boolean tryAcquire(int arg) {
+            return super.tryAcquire(arg);
+        }
+
+        @Override
+        protected boolean tryRelease(int arg) {
+            return super.tryRelease(arg);
+        }
+
+        @Override
+        protected boolean isHeldExclusively() {
+            return getState() == 1;
+        }
+    }
+    @Override
+    public void lock() {
+
+    }
+
+    @Override
+    public void lockInterruptibly() throws InterruptedException {
+
+    }
+
+    @Override
+    public boolean tryLock() {
+        return false;
+    }
+
+    @Override
+    public boolean tryLock(long time, TimeUnit unit) throws InterruptedException {
+        return false;
+    }
+
+    @Override
+    public void unlock() {
+
+    }
+
+    @Override
+    public Condition newCondition() {
+        return null;
+    }
+}
