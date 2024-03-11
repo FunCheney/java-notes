7168ab780c657381135290ae920c40949550fadf
Fchen <fc_2012@163.com>
2024-03-11 23:16:53 +0800

    feat: 字节码文件

---
+++ src/main/java/com/fchen/jdk/demo/method/Demo_03.java
@@ -0,0 +1,13 @@
+package com.fchen.jdk.demo.method;
+
+public class Demo_03 {
+    public static void main(String[] args) {
+        int i = 0, j = 0, k = 0;
+        i++;
+        j = j + 1;
+        k +=  1;
+        System.out.println(i);
+        System.out.println(j);
+        System.out.println(k);
+    }
+}
