36402f64b7d608b1902c575a23a9a3162580acae
Fchen <fc_2012@163.com>
2024-03-25 08:20:43 +0800

    feat: 类的初始化阶段

---
+++ src/main/java/com/fchen/jdk/classAndObj/InitializationTest.java
@@ -0,0 +1,15 @@
+package com.fchen.jdk.classAndObj;
+
+/**
+ *
+ * 过程三：初始化阶段
+ */
+public class InitializationTest {
+    public static int id = 1;
+    public static int number;
+
+    static {
+        number = 2;
+        System.out.println("father static{}");
+    }
+}
---
+++ src/main/java/com/fchen/jdk/classAndObj/SubInitialization.java
@@ -0,0 +1,14 @@
+package com.fchen.jdk.classAndObj;
+
+/**
+ */
+public class SubInitialization extends InitializationTest {
+    static{
+        number = 4;//number属性必须提前已经加载：一定会先加载父类。
+        System.out.println("son static{}");
+    }
+
+    public static void main(String[] args) {
+        System.out.println(number);
+    }
+}
