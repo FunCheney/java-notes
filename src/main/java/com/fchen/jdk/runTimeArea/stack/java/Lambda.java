676467e8f2298bac36ecbeaced1b19e9609486e1
Fchen <fc_2012@163.com>
2024-04-09 21:48:05 +0800

    feat: method

---
+++ src/main/java/com/fchen/jdk/runTimeArea/stack/java/Lambda.java
@@ -0,0 +1,30 @@
+package com.fchen.jdk.runTimeArea.stack.java;
+
+/**
+ * 体会invokedynamic指令
+ */
+@FunctionalInterface
+interface Func {
+    public boolean func(String str);
+}
+
+public class Lambda {
+    public void lambda(Func func) {
+        return;
+    }
+
+    public static void main(String[] args) {
+        Lambda lambda = new Lambda();
+
+        Func func = s -> {
+            return true;
+        };
+
+        lambda.lambda(func);
+
+        lambda.lambda(s -> {
+            return true;
+        });
+    }
+}
+
