d0e0204470e4ac83539dcdf9a5ed8786b97cdd4a
Fchen <fc_2012@163.com>
2024-04-13 23:33:08 +0800

    feat: obj alloc

---
+++ src/main/java/com/fchen/jdk/runTimeArea/heap/java2/SynchronizedTest.java
@@ -0,0 +1,13 @@
+package com.fchen.jdk.runTimeArea.heap.java2;
+
+/**
+ * 同步省略说明
+ */
+public class SynchronizedTest {
+    public void f() {
+        Object hollis = new Object();
+        synchronized(hollis) {
+            System.out.println(hollis);
+        }
+    }
+}
